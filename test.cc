4715b87dda1d07deebf36750d42fefc24ab6a0e6
leiliang <leiliang4u@126.com>
2020-04-09 17:44:14 +0800

    add test_leveldb UT.

--- test.cc
+++ test.cc
@@ -1,7 +1,30 @@
+#include "include/leveldb/db.h"
 #include <iostream>
+#include <string.h>
+#include <assert.h>
+
+void test_leveldb() {
+    leveldb::DB* db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options,"/tmp/test", &db)
+    assert(status.ok());
+
+    std::string key="leil";
+    std::string value = "666";
+
+    status = db->Put(leveldb::WriteOptions(), key,value);
+    assert(status.ok());
+
+    status = db->Get(leveldb::ReadOptions(), key, &value);
+    assert(status.ok());
+
+    std::cout << key <<"'s value is:" << value << std::endl;
+    delete db;
+}
 
 int main() {
     std::cout << "Hello levelDB !" << std::endl;
-
+    //test_leveldb();
     return 0;
 }
