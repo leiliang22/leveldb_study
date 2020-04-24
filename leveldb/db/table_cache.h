2ef69e8d54abfa723ca625648c890963bcb7852b
leiliang22 <leiliang835@163.com>
2020-04-24 10:34:22 +0800

    add TableCache

--- leveldb/db/table_cache.h
+++ leveldb/db/table_cache.h
@@ -4,13 +4,36 @@
 #include <string>
 
 #include "db/dbformat.h"
+#include "leveldb/cache.h"
+#include "leveldb/table.h"
+#include "port/port.h"
 
 namespace leveldb {
 
 class Env;
 
 class TableCache {
-    
+public:
+    TableCache(const std::string& dbname, const Options& options, int entries);
+    ~TableCache();
+
+    Iterator* NewIterator(const ReadOptions& options, uint64_t file_number,
+                          uint64_t file_size, Table** tableptr = nullptr);
+
+    Status Get(const ReadOptions& options, uint64_t file_number,
+               uint64_t file_size, const Slice& k, void* arg,
+               void (*handle_result)(void*, const Slice&, const Slice&));
+
+    // Evict any entry for the specified file number
+    void Evict(uint64_t file_number);
+
+private:
+    Status FindTable(uint64_t file_number, uint64_t file_size, Cache::Handle**);
+
+    Env* const env_;
+    const std::string dbname_;
+    const Options& options_;
+    Cache* cache_;
 };
 
 
---
+++ leveldb/include/leveldb/table.h
@@ -0,0 +1,13 @@
+#ifndef STORAGE_LEVELDB_INCLUDE_TABLE_H_
+#define STORAGE_LEVELDB_INCLUDE_TABLE_H_
+
+#include <stdint.h>
+
+#include "leveldb/export.h"
+#include "leveldb/iterator.h"
+
+namespace leveldb {
+
+}  // namespace leveldb
+
+#endif  // STORAGE_LEVELDB_INCLUDE_TABLE_H_
