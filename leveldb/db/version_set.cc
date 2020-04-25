9b12aea50fb0f4f01373707b50c2d8cf917b4a4b
leiliang22 <leiliang835@163.com>
2020-04-25 12:20:57 +0800

    add Version::Get

--- leveldb/db/version_set.cc
+++ leveldb/db/version_set.cc
@@ -3,6 +3,99 @@
 
 namespace leveldb {
 
+
+namespace {
+enum SaverState {
+    kNotFound,
+    kFound,
+    kDeleted,
+    kCorrupt,
+};
+struct Saver {
+    SaverState state;
+    const Comparator* ucmp;
+    Slice user_key;
+    std::string* value;
+};
+}  // namespace
+
+Status Version::Get(const ReadOptions& options, const LookupKey& k,
+                    std::string* value, GetStats* stats) {
+    stats->seek_file = nullptr;
+    stats->seek_file_level = -1;
+
+    struct State {
+        Saver saver;
+        GetStats* stats;
+        const ReadOptions* options;
+        Slice ikey;
+        FileMetaData* last_file_read;
+        int last_file_read_level;
+
+        VersionSet* vset;
+        Status s;
+        bool found;
+
+        static bool Match(void* arg, int level, FileMetaData* f) {
+            State* state = reinterpret_cast<State*>(arg);
+
+            if (state->stats->seek_file == nullptr &&
+                    state->last_file_read != nullptr) {
+                // we have had more than one seek for this read. charge the 1st file
+                state->stats->seek_file = state->stats->last_file_read;
+                state->stats->seek_file_level = state->last_file_read_level;
+            }
+
+            state->last_file_read = f;
+            state->last_file_read_level = level;
+
+            state->s = state->vset->table_cache_->Get(*state->options, f->number,
+                                                      f->file_size, state->ikey,
+                                                      &state->saver, SaveValue);
+            if (!state->s.ok()) {
+                state->found = true;
+                return false;
+            }
+            switch (state->saver.state) {
+                case kNotFound:
+                    return true;  // keep search in other files
+                case kFound:
+                    state->found = true;
+                    return false;
+                case kDeleted:
+                    return false;
+                case kCorrupt:
+                    state->s = Status::Corruption("corrupted key for ", state->saver.user_key);
+                    state->found = true;
+                    return false;
+            }
+
+            // not reached. added to avoid false compilation warnings of 
+            // "control reaches end of non-void function".
+            return false;
+        }
+    };
+
+    State state;
+    state.found = false;
+    state.stats = stats;
+    state.last_file_read = nullptr;
+    satte.last_file_read_level = -1;
+
+    state.options = &options;
+    state.ikey = k.internal_key();
+    state.vset = vset_;
+
+    state.saver.state = kNotFound;
+    state.saver.ucmp = vset_->icmp_.user_comparator();
+    state.saver.user_key = k.user_key();
+    state.saver.value = value;
+
+    ForEachOverlapping(state.saver.user_key, state.ikey, &state, &State::Match);
+
+    return state.found ? state.s : Status::NotFound(Slice());
+}
+
 // a helper class so we can efficiently apply a whole sequence of
 // edits to a particular state without creating intermdiate Versions 
 // that contain full copies of the in
